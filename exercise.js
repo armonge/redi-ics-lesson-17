7e25a933dc659335bfda867a86888445aa3ae31f
Andres Reyes Monge <armonge@gmail.com>
2023-05-22 09:45:33 +0000

    Updates the exercise

--- exercise.js
+++ exercise.js
@@ -1,26 +1,23 @@
 const submit = document.querySelector('input[type="submit"]');
-submit.addEventListener("click", (event) => {
-    //  Already a hint here!
-});
+const ul = document.querySelector('ul');
+const input = document.querySelector('input.shoppinglist-input')
+
+const removeClickedListener = (event) => {
+    // Stops the page from changing
+    event.preventDefault();
 
 
+    // Add your removeChild code here
+    // ...
+}
 
+const submitListener = (event) => {
+    // Stops the page from changing
+    event.preventDefault();
 
+    // Add your createElement code here
+    // ...
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+submit.addEventListener("click", submitListener);
+document.querySelector('li').addEventListener('click', removeClickedListener)
